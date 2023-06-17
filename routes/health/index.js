4640f86d8c4239c721aba74a0871d58b35b8e420
Theo <tmoanawong@bimedia.com>
2023-06-18 00:35:37 +0200

    basic set up

---
+++ routes/health/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// eslint-disable-next-line no-unused-vars
+module.exports = async function(fastify, opts) {
+    fastify.get('/', async function(/* request, reply */) {
+        return { health: 'OK' };
+    });
+};
