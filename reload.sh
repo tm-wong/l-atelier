d2f1fa49393000d2cb2742ac9660a3fa5fd1d35f
Theo <genaddress@gmail.com>
2023-11-15 16:44:21 +0100

    added reload.sh into .gitignore

--- reload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker container rm app
-docker image rm l-atelier-ts-app
-sudo chown -R theo: /home/theo/Documents/theo-perso/l-atelier/l-atelier-ts/postgres-data/data
-docker-compose up --build
-
